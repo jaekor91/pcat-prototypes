71253db2ff68eb8a433e5c276d689346a53d67a1
Jae Hyeon Lee <jaekor91@gmail.com>
2017-07-24 11:41:36 -0400

    First working version. Star insert only.

--- model_evaluation_concurrent_base.c
+++ model_evaluation_concurrent_base.c
@@ -55,16 +55,24 @@
 #include <sys/mman.h>
 
 
-#define NPIX 25
-#define NPIX2 (NPIX*NPIX)
+
 #define INNER 10
 #define AVX_CACHE 16 // Number of floats that can fit into AVX512
-#define BLOCK 32 
+#define NPIX 2 * AVX_CACHE
+#define NPIX2 (NPIX*NPIX)
+#define BLOCK 2 * AVX_CACHE 
 #define MARGIN 8
 #define REGION 16
 #define NUM_BLOCKS_PER_DIM 16 // Note that if the image size is too big, then the computer may not be able to hold. 
-#define NITER 1000 // Number of iterations
+								// +1 for the extra padding. We only consider the inner blocks.
+#define NUM_BLOCKS_PER_DIM_W_PAD (NUM_BLOCKS_PER_DIM+2) // Note that if the image size is too big, then the computer may not be able to hold. 
+#define NITER_BURNIN 50
+#define NITER (100+NITER_BURNIN) // Number of iterations
 #define LARGE_LOGLIKE 1000 // Large loglike value filler.
+#define BYTES 4
+#define MAX_STARS AVX_CACHE
+#define IMAGE_WIDTH (NUM_BLOCKS_PER_DIM_W_PAD * BLOCK)
+#define IMAGE_SIZE IMAGE_WIDTH * IMAGE_WIDTH
 
 
 void init_mat_float(float* mat, int size, float fill_val, int rand_fill)
@@ -89,6 +97,50 @@ void init_mat_float(float* mat, int size, float fill_val, int rand_fill)
 }
 
 
+void init_mat_int(int* mat, int size, int min, int max)
+{
+	// Fill the integer matrix with random number in [min, max)
+	int i;
+	int diff = max-min;
+	for (i=0; i<size; i++){
+		mat[i] = (rand() % diff) + min;
+	}
+	return;
+}
+
+void generate_parity(int* parity_vec, int vec_size){
+	// Given the vector parity_vec of size NITER,
+	// draw random 0 or 1 for NITER times and save.
+	int i;
+	for (i=0; i<vec_size; i++){
+		parity_vec[i] = flip_coin();
+	}
+	return;
+}
+
+int flip_coin(){
+	// Generate 0 or 1 randomly.
+	int half_point = RAND_MAX / 2;
+	int coin;
+	int x = rand();
+	if (x>half_point){
+		coin = 1;
+	}
+	else{
+		coin = 0;
+	}
+	return coin;
+}
+
+void print_mat_int(int* mat, int size){
+	int i;
+	for (i=0; i<size-1; i++){
+		printf("%d, ", mat[i]);
+	}
+	printf("%d\n", mat[size-1]);
+}
+
+
 
 int main(int argc, char *argv[])
 {	
@@ -96,27 +148,21 @@ int main(int argc, char *argv[])
 	srand(123); 
 
 	// ----- Declare global, shared variables ----- //
-	// Loop variables
-	// int i, j, k, l;
-
 	// Number of stars
-	int nstar[11] = {1, 2, 3, 4, 5, 10, 50, 100, 500, 1000, 5000};
+	int nstar[6] = {1, 2, 3, 4, 8, 16};
+	int size_of_nstar = 6;
 
- 	// Number of iteration to perform.
- 	int niter = 1000;
 
 	// * Pre-allocate image DATA, MODEL, design matrix, num_stars, and loglike
-	int size_of_DATA = (NUM_BLOCKS_PER_DIM * BLOCK) * (NUM_BLOCKS_PER_DIM * BLOCK);
-
-	int size_of_A = (4 * AVX_CACHE * AVX_CACHE) * INNER;
-	int size_of_LOGLIKE = NUM_BLOCKS_PER_DIM * NUM_BLOCKS_PER_DIM * AVX_CACHE; // Padding to avoid cache coherence issue.
+	int size_of_DATA = IMAGE_SIZE;
+	int size_of_A = NPIX2 * INNER;
+	int size_of_LOGLIKE = NUM_BLOCKS_PER_DIM_W_PAD * NUM_BLOCKS_PER_DIM_W_PAD * AVX_CACHE; // Padding to avoid cache coherence issue.
 	__attribute__((aligned(64))) float DATA[size_of_DATA];
 	__attribute__((aligned(64))) float MODEL[size_of_DATA];
 	__attribute__((aligned(64))) float A[size_of_A];
 	__attribute__((aligned(64))) float LOGLIKE[size_of_LOGLIKE]; 
-
-	printf("Image size %d\n", size_of_DATA);
-
+	printf("Image size: %d\n", size_of_DATA);
+	printf("Image width: %d\n", IMAGE_WIDTH);
 
 	// ----- Initialize global, shared variables ----- //
 	init_mat_float(DATA, size_of_DATA, 0.0, 1); // Fill data with random values
@@ -124,133 +170,135 @@ int main(int argc, char *argv[])
 	init_mat_float(A, size_of_A, 0.0, 1); // Fill data with random values
 	init_mat_float(LOGLIKE, size_of_LOGLIKE, LARGE_LOGLIKE, 0); // Fill data with random values
 
-// - Loops:
-//		1) For num_stars {1, 2, 3, 4, 5, 10, 50, 100, 500, 1000, 5000}
-//			2) For each iteration of loop. Randomly generate.
-//				* X, Y [NUM_BLOCKS_PER_DIM^2, num_stars]: The (x,y) position of stars. Defined within each block. 
-// 					The upper-left corner has (0,0) coordintate.
-//				* dX, dY [NUM_BLOCKS_PER_DIM^2 x num_stars, INNER]: Floats [0, 1].
-//				* f: Floats [0, 256].
-//				* parity_x, parity_y: 0 or 1. Determine which sub-blocks to work on.
-// 				// For later: * PSFs [num_stars * NUM_BLOCKS_PER_DIM^2, (2 x AVX_CACHE)^2]: Storage for the PSFs 
-// 				* start: Begin timing
-//				3) For each block chosen by the xy-parities (let each thread work on)
-// 					* Compute the star PSFs by multiplying the design matrix with the appropriate portion of dX. 
-// 					Insert without intermitten storage. Make sure to multiply by the new flux. 
-// 					(Note that killing a star is like giving a negative flux.)
-// 					* Compute the new likelihood based on the updated model. Based on 48 x 48 region, region larger than the block.
-// 					* Compare to the old likelihood and if the new value is smaller then update the loglike and continue.
-// 					If bigger then undo the addition by subtracting what was added to the model image.
-//				* end: End timing.
-// 				* Compute dt and add to dT
-//			Report the speed in terms of wall time.
 
-	// // Allocate memory with aligned arrays
-	// // Matrix multiplication takes the form dX x A = PSF
-	// // dX [nstar, INNER] 
-	// // A [INNER, NPIX2] 
-	// // PSF [nstar, NPIX2]
-	// int size_of_dX = INNER*nstar;
-	// int size_of_A = NPIX2*INNER;
-	// int size_of_PSF = NPIX2*nstar;
-	// __attribute__((aligned(64))) float dX[size_of_dX];
-	// __attribute__((aligned(64))) float A[size_of_A];
-	// __attribute__((aligned(64))) float PSF[size_of_PSF];
+	// ----- Pre-allocate memory for within-loop shared variables ----- //
+	// * Pre-allocate space for X, Y, dX, dY, F, parity_X, parity_Y.
+	int size_of_XYF = (NUM_BLOCKS_PER_DIM_W_PAD * NUM_BLOCKS_PER_DIM_W_PAD) * MAX_STARS; // Max number of stars is 16. Each block gets 16 floating point.
+	int size_of_dX = (NUM_BLOCKS_PER_DIM_W_PAD * NUM_BLOCKS_PER_DIM_W_PAD) * MAX_STARS * AVX_CACHE; // Each block gets MAX_STARS * AVX_CACHE. Note, however, only the first 10 elements matter.
+	__attribute__((aligned(64))) int X[size_of_XYF]; // Assume 4 bytes integer
+	__attribute__((aligned(64))) int Y[size_of_XYF];
+	__attribute__((aligned(64))) float F[size_of_XYF];
+	__attribute__((aligned(64))) float dX[size_of_dX];
 
+	// ----- Pre-draw parity variables ----- //
+	int parity_X[NITER];
+	int parity_Y[NITER];
+	generate_parity(parity_X, NITER);
+	generate_parity(parity_Y, NITER);
+	// print_mat_int(parity_X, NITER);
+	
+	// ----- Main computation begins here ----- //
+	int i, j; // Loop variables
+	int ibx, iby; // Block idx
+	int ns; // Number of stars
+	int par_X, par_Y;
+	double start, end, dt, dt_per_iter;
+	// For each number of stars.
+	for (i=0; i<size_of_nstar; i++){
+		ns = nstar[i];
+		dt = 0;
+		// Start of the loop
+		for (j=0; j<NITER; j++){
+			// Pick parity
+			par_X = parity_X[j];
+			par_Y = parity_Y[j];
+			// par_Y = 0;
+			// printf("Parity: (%d, %d)\n", par_X, par_Y);
 
-	// Initialize the shared variables
-	// init_mat(dX, size_of_dX, 1);
-	// init_mat(A, size_of_A, 1);
-	// init_mat(PSF, size_of_PSF, 0);
+			// Randomly generate X, Y, dX, dY, F
+			init_mat_float(dX, size_of_dX, 0.0, 1); 
+			init_mat_float(F, size_of_XYF, 0.0, 1); 
+			init_mat_int(X, size_of_XYF, 0, MARGIN);
+			init_mat_int(Y, size_of_XYF, 0, MARGIN);			
 
- // 	struct timeval time_begin, time_end;
- // 	double elapsed_time;
-	// printf("Evaluating matrix vector multplication %d times.\n", count); fflush(stdout);
-	// // General strategy:
-	// // Multiply each row of the design matrix A with the appropriate element of dX and add the result
-	// // to the resulting vector, or PSF, which acts as the accumulator. 
+			// print_mat_int(X, size_of_XYF); // Used to check the values of the matrix X, Y.
 
-	// // burn-in
-	// // Compute in 16 chunks
-	// // Won't work if NPIX2 is not divisible by 16. 
-	// for (l=0; l<count; l++){
-	// 	#pragma omp parallel for
-	// 	for (k=0; k<nstar; k++){
-	// 		#pragma omp simd
-	// 		for (j=0; j<NPIX2; j++){
-	// 			for (i=0; i<INNER; i++){
-	// 				// #pragma omp atomic
-	// 				PSF[k*NPIX2+j] += dX[INNER*k+i] * A[i*NPIX2+j];
-	// 			}
-	// 		}
-	// 	}
-	// }
+			start = omp_get_wtime(); // Timing starts here
+			// ----- Model evaluation, followed by acceptance or rejection. ----- //
+			// Iterating through all the blocks.
+			// IMPORTANT: X is the row direction and Y is the column direction.
+			#pragma omp parallel
+			{
+				// Recall that we only consider the center blocks. That's where the extra 1 come from
+				#pragma omp for collapse(2)
+				for (iby=1+par_Y; iby<NUM_BLOCKS_PER_DIM_W_PAD-1; iby+=2){ // Column direction				
+					for (ibx=1+par_X; ibx<NUM_BLOCKS_PER_DIM_W_PAD-1; ibx+=2){ // Row direction
+						int k, l, m; // private loop variables
+						int block_ID = ibx * NUM_BLOCKS_PER_DIM_W_PAD + iby; // (0, 0) corresponds to block 0, (0, 1) block 1, etc.
+						// printf("Block ID: %3d, (bx, by): %3d, %3d\n", block_ID, ibx, iby); // Used to check whether all the loops are properly addressed.
 
-	// // sum_10 (compute 625)
-	// // for (l=0; l<count; l++){
-	// // 	for (k=0; k<nstar; k++){
-	// // 		for (i=0; i<INNER; i++){
-	// // 			// xel = dX[INNER*k+i];
-	// // 			// #pragma omp parallel for simd
-	// // 			// #pragma omp simd				
-	// // 			for (j=0; j<NPIX2; j++){
-	// // 				PSF[k*NPIX2+j] += dX[INNER*k+i] * A[i*NPIX2+j];
-	// // 			}
-	// // 		}
-	// // 	}
-	// // }
+						// Read into cache necessary values. (Need to verify)
+						float b_loglike = LOGLIKE[block_ID * AVX_CACHE];// Loglikelihood corresponding to the block.
+						float p_loglike = 0; // Proposed move's loglikehood
 
-	// // Unnecessary optimization. Old.
-	// // float xel; //current dX element
-	// // int psf_row;
-	// // int A_row;
-	// // int dx_row;
-	// // for (l=0; l<count; l++){
-	// // 	for (k=0; k<nstar; k++){
-	// // 		psf_row = k*NPIX2;
-	// // 		dx_row = INNER*k;
-	// // 		for (i=0; i<INNER; i++){
-	// // 			xel = dX[dx_row+i];
-	// // 			A_row = i*NPIX2;
-	// // 			for (j=0; j<NPIX2; j++){
-	// // 				PSF[psf_row+j] += xel * A[A_row+j];
-	// // 			}
-	// // 		}
-	// // 	}
-	// // }
+						// // try aligning? Would it be faster? 
+						// Manual pre-fetching might be bad...
+						// __attribute__((aligned(64))) float p_dX[AVX_CACHE * ns];
+						// __attribute__((aligned(64))) float p_dY[AVX_CACHE * ns];
+						// __attribute__((aligned(64))) float p_F[MAX_STARS];
+						// __attribute__((aligned(64))) int p_X[MAX_STARS];
+						// __attribute__((aligned(64))) int p_Y[MAX_STARS]
+						// Read into cache
+						// for (k=0; k<MAX_STARS; k++){
+						// }						
 
-	// gettimeofday(&time_begin, NULL);
-	// // Compute in 16 chunks
-	// // Won't work if NPIX2 is not divisible by 16. 
-	// for (l=0; l<count; l++){
-	// 	#pragma omp parallel		
-	// 	#pragma omp for
-	// 	for (k=0; k<nstar; k++){
-	// 			#pragma omp simd
-	// 			for (j=0; j<NPIX2; j++){
-	// 				for (i=0; i<INNER; i++){
-	// 					// #pragma omp atomic						
-	// 					PSF[k*NPIX2+j] += dX[INNER*k+i] * A[i*NPIX2+j];
-	// 				}
-	// 			}
-	// 		}
-	// }
-	// gettimeofday(&time_end, NULL);
+						// Start index for X, Y, F and dX, dY
+						int idx_XYF = block_ID * MAX_STARS;
+						int idx_dX = block_ID * MAX_STARS * AVX_CACHE;
+						// row and col location of the star based on X, Y values.
+						int idx_row; 
+						int idx_col;						
+						int psf_width = NPIX; // Not sure why simply using NPIX rather than a variable.
+						// Update the model by inserting ns stars
+						// Compute the star PSFs by multiplying the design matrix with the appropriate portion of dX. 
+						// Insert without intermitten storage. Make sure to multiply by the new flux. 
+						// (Note that killing a star is like giving a negative flux.)
 
+						for (k=0; k<ns; k++){
+							idx_row = ibx * BLOCK + MARGIN + X[idx_XYF+k];
+							idx_col = iby * BLOCK + MARGIN + Y[idx_XYF+k];
+							// // Check whether the row and col look okay
+							// if (k==0){
+							// 	printf("idx_row,col: %5d, %5d\n", idx_row, idx_col);
+							// }
 
+							// Compute in 16 chunks. Won't work if NPIX2 is not divisible by 16.
+							#pragma omp simd
+							for (l=0; l<NPIX2; l++){
+								for (m=0; m<INNER; m++){
+									MODEL[(idx_row+l/psf_width)*IMAGE_WIDTH + (idx_col+l%NPIX)] += F[idx_XYF+k] * dX[idx_dX+k*AVX_CACHE+m] * A[m*NPIX2+l];
+								} 
+							}
+						}
 
-	// // Calculate the elapsed time and caculate flops
-	// elapsed_time = (time_end.tv_sec - time_begin.tv_sec) + (time_end.tv_usec - time_begin.tv_usec)*1.0e-6;
-	// double sec = (time_end.tv_sec - time_begin.tv_sec);
-	// double usec = (time_end.tv_usec - time_begin.tv_usec);	
-	// printf("sec: %.3f\n", sec);
-	// printf("usec: %.3f\n", usec);
-	// double flops = 2*NPIX2*INNER*nstar;
-	// printf("flops: %.1f\n", flops);
+						// Compute the loglikelihood
+		// 					* Compute the new likelihood based on the updated model. Based on 48 x 48 region, region larger than the block.
 
-	// double Gflops = ((2*NPIX2*INNER*nstar)/(elapsed_time/count)) * (1.0e-09);
-	// // Report
-	// printf("Elapsed time : %.3f (s)\n", elapsed_time);
-	// printf("FLOPS        : %.3f (GFlops)\n", Gflops);
+						// Compare and decide whether to update the model or not.
+		// 					* Compare to the old likelihood and if the new value is smaller then update the loglike and continue.
+		// 					If bigger then undo the addition by subtracting what was added to the model image.						
+						if (p_loglike > b_loglike){
+							// Undo
+						}
+						else{
+							// Accept the proposal
+							LOGLIKE[block_ID * AVX_CACHE] = p_loglike;// Loglikelihood corresponding to the block.							
+						}
 
+					} // End of y block loop
+				} // End of x block loop
+			}// End of OMP parallel section
+
+			end = omp_get_wtime();
+			// Update time only if burn in has passed.
+			if (j>NITER_BURNIN){
+				dt += (end-start);
+			}
+		} // End of NITER loop
+
+	// Calculatin the time took.
+	dt_per_iter = (dt / (NITER-NITER_BURNIN)) * (1e06); // Burn-in	
+	// dt_per_iter = (dt / NITER) * (1e06); // Actual	
+	printf("ns =%5d, elapsed time per iter (us): %.3f\n", ns, dt_per_iter);
+	} // End of nstar loop
 }
